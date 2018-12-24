7e5e0f6865b509a776e1daf797c421f5dc10a446
Pierre Tondereau <pierre.tondereau@protonmail.com>
2018-12-24 12:29:52 +0100

    Fix deprecated method call on twig

--- src/JMS/Serializer/Twig/SerializerExtension.php
+++ src/JMS/Serializer/Twig/SerializerExtension.php
@@ -43,14 +43,14 @@ class SerializerExtension extends \Twig_Extension
     public function getFilters()
     {
         return array(
-            'serialize'      => new \Twig_Filter_Method($this, 'serialize'),
+            'serialize'      => new \Twig_SimpleFilter($this, 'serialize'),
         );
     }
 
     public function getFunctions()
     {
         return array(
-            'serialization_context' => new \Twig_Function_Method($this, 'createContext'),
+            'serialization_context' => new \Twig_SimpleFunction($this, 'createContext'),
         );
     }
 
